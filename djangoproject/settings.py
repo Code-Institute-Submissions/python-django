9826a48f02e95bc0e2c3cd480a0c9cd944f065b6
anthony keogh <siliconirish@gmal.com>
2020-04-23 15:56:43 +0100

    Meaningful messager goes here

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -165,8 +165,9 @@ STATICFILES_DIRS = (
 
 ##STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 from django.urls import reverse_lazy
 
