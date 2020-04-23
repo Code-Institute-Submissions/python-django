533e63392672212f8fe3b01a45d285fc160e4e28
anthony keogh <siliconirish@gmal.com>
2020-04-23 21:14:44 +0100

    Meaningful messager goes here

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #SECRET_KEY = 'xg(k7p#ka=3h&g*27ux2ky265qk&y3#g-og*^vk+-=6l8l7l1k'
 
 if os.path.isfile('env.py'):
-    SECRET_KEY = os.getenv('mysecretkeydjango')
+    SECRET_KEY = os.getenv('SECRET_KEY')
 else:
     SECRET_KEY = 'thesecretkey'
 
@@ -98,7 +98,7 @@ WSGI_APPLICATION = "djangoproject.wsgi.application"
 
 if "DATABASE_URL" in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.getenv('postgres://rjzkipmlcqkjma:19ac670cf0ca543ac07eee721cba098e6c85897935e0a3a9759c73ef81454ccf@ec2-54-247-169-129.eu-west-1.compute.amazonaws.com:5432/d87e8lb1pralf4'))
+        'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
     }
 else:
     print("Postgres URL not found, using sqlite instead")
